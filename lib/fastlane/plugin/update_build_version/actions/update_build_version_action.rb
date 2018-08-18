72dcb0f4eee71574f66a1e59158997a4b6c36219
陈军志 <chenjunzhi@parkingwang.com>
2018-08-18 18:03:41 +0800

    修改说明

--- lib/fastlane/plugin/update_build_version/actions/update_build_version_action.rb
+++ lib/fastlane/plugin/update_build_version/actions/update_build_version_action.rb
@@ -5,31 +5,31 @@ module Fastlane
   module Actions
     class UpdateBuildVersionAction < Action
       def self.run(params)
-        last_git_commit = Actions.sh('git log --pretty=format:"%h" -1')
+
+        ## 获取最后的commit
+        last_git_commit = Actions.last_git_commit_formatted_with('%h')
+
+        ##格式化时间
         time = Time.now.strftime("%y%M%d")
+
+        ##组装新的版本号
         new_version = time + "g" + last_git_commit
-        puts (last_git_commit)
-        UI.message("last_git_commit #{last_git_commit}")
+        
         # command = "avgtool bump -all  #{new_version}"
-        # Fastlane::Actions::IncrementBuildNumberAction.run(build_number: new_version)
+        Fastlane::Actions::IncrementBuildNumberAction.run(build_number: new_version)
         UI.message("The update_build_version plugin is working!")
       end
 
       def self.description
-        "Append git hash value to build version"
+        "更新本地的版本号，使测试版本更加容易找到基于哪个commit发布的"
       end
 
       def self.authors
-        ["陈军志"]
-      end
-
-      def self.return_value
-        # If your method provides a return value, you can describe here what it does
+        ["ChenJzzz"]
       end
 
       def self.details
-        # Optional:
-        "for example : 18101014g[git_versio] , just for IRain"
+        "格式：[日期]g[最后一次commit的hash值]， 例子：180815g008fa5"
       end
 
       def self.available_options
@@ -43,10 +43,7 @@ module Fastlane
       end
 
       def self.is_supported?(platform)
-        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
-        # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
-        #
-        # [:ios, :mac, :android].include?(platform)
+        [:ios].include?(platform)
         true
       end
     end
